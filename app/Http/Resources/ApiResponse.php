4a5b1d17d716a7f552ed9d59ae8756f82911b4b9
rangeldheo <rangeldheo@gmail.com>
2020-07-26 00:36:22 -0300

    Resource ApiResponse criada e configurada

--- app/Http/Resources/ApiResponse.php
+++ app/Http/Resources/ApiResponse.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Support\Facades\Response;
 
-class ApiResponse extends JsonResource
+class ApiResponse 
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
+    public static function return($data = [], $error = [], $message = [], $statusCode = null)
     {
-        return parent::toArray($request);
+        $objResponse = new HttpResponse();
+        return Response::json([
+            'http_status_code'=> $statusCode ??  $objResponse->getStatusCode(),
+            'data'            => $data,
+            'error'           => $error,
+            'message'          => $message,
+        ]);
     }
 }
